cffa9a2fffcc75ebfc4a9ec6d3462e52e83b4884
Truthnuggets <martincattell1618@outlook.com>
2019-09-27 19:42:36 +0100

    Create function for replacing button with number

--- Minesweeper.py
+++ Minesweeper.py
@@ -4,8 +4,11 @@ main_window = Tk()
 
 
 class MineButton(object):
-    def __init__(self):
+    def __init__(self, row, column):
+        self.row = row
+        self.column = column
         self.button = Button(main_window, width=2, height=1)
+
 
 
 
@@ -16,14 +19,26 @@ def create_grid(width, height):
     for i in range(height):
         row_list = []
         for j in range(width):
-            b = MineButton()
+            b = MineButton(i, j)
+            b.button.bind("<Button-1>", button_function)
             row_list.append(b)
             row_list[j].button.grid(row=i, column=j)
         grid_list.append(row_list)
     return grid_list
 
 
-create_grid(10, 10)
+
+
+
+
+
+def button_function(event):
+    label = Label(main_window, text=1)
+    grid[0][0].button.grid_remove()
+    label.grid(row=0)
+
+grid = create_grid(10, 10)
+
 
 
 
