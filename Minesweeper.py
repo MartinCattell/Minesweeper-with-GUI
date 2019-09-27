23902d3bc51e6d9c6d5ed5169547055c8366acad
Truthnuggets <martincattell1618@outlook.com>
2019-09-27 20:54:33 +0100

    Update button function to place number in corresponding button press

--- Minesweeper.py
+++ Minesweeper.py
@@ -7,7 +7,7 @@ class MineButton(object):
     def __init__(self, row, column):
         self.row = row
         self.column = column
-        self.button = Button(main_window, width=2, height=1)
+        self.button = Button(main_window, width=2, height=1, command=lambda r = self.row, c=self.column: button_function(r, c))
 
 
 
@@ -20,24 +20,19 @@ def create_grid(width, height):
         row_list = []
         for j in range(width):
             b = MineButton(i, j)
-            b.button.bind("<Button-1>", button_function)
             row_list.append(b)
             row_list[j].button.grid(row=i, column=j)
         grid_list.append(row_list)
     return grid_list
 
 
+grid = create_grid(15, 15)
 
 
-
-
-
-def button_function(event):
+def button_function(row, column):
     label = Label(main_window, text=1)
-    grid[0][0].button.grid_remove()
-    label.grid(row=0)
-
-grid = create_grid(10, 10)
+    grid[row][column].button.grid_remove()
+    label.grid(row=row, column=column)
 
 
 
