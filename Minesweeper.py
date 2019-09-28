4b90c34d6a8f9b6a9d06272fdeae5de9809c7fd5
Truthnuggets <martincattell1618@outlook.com>
2019-09-28 01:00:55 +0100

    Trouble with recurring mine-free spaces

--- Minesweeper.py
+++ Minesweeper.py
@@ -14,7 +14,7 @@ class MineButton(object):
 
 grid_width = 15
 grid_height = 15
-bomb_percentage = 2
+bomb_percentage = 10
 
 
 def bomb_number(height, width, percent):
@@ -107,39 +107,76 @@ mines_list = plant_mines(grid_height, grid_width, bomb_percentage)
 
 
 def button_function(row, column):
-    label0 = Label(main_window, text="")
-    label1 = Label(main_window, text=1, fg="#FF8C00")
-    label2 = Label(main_window, text=2, fg="#FF0000")
-    label3 = Label(main_window, text=3, fg="#006400")
-    label4 = Label(main_window, text=4, fg="#008B8B")
-    label5 = Label(main_window, text=5, fg="#191970")
-    label6 = Label(main_window, text=6, fg="#8B4513")
-    label7 = Label(main_window, text=7, fg="#696969")
-    label8 = Label(main_window, text=8, fg="#000000")
-    count = 0
-    surround_list = surround_mine([row, column], grid_height, grid_width)
-    for i in surround_list:
-        if i in mines_list:
-            count += 1
     grid[row][column].button.grid_remove()
-    if count == 0:
-        label0.grid(row=row, column=column)
-    if count == 1:
-        label1.grid(row=row, column=column)
-    if count == 2:
-        label2.grid(row=row, column=column)
-    if count == 3:
-        label3.grid(row=row, column=column)
-    if count == 4:
-        label4.grid(row=row, column=column)
-    if count == 5:
-        label5.grid(row=row, column=column)
-    if count == 6:
-        label6.grid(row=row, column=column)
-    if count == 7:
-        label7.grid(row=row, column=column)
-    if count == 8:
-        label8.grid(row=row, column=column)
+    label_mine = Label(main_window, text="M", font=("none", 10, "bold"))
+    if [row, column] in mines_list:
+        label_mine.grid(row=row, column=column)
+        return True
+    else:
+        label0 = Label(main_window, text="O")
+        label1 = Label(main_window, text=1, fg="#FF8C00", font=("none", 10, "bold"))
+        label2 = Label(main_window, text=2, fg="#FF0000", font=("none", 10, "bold"))
+        label3 = Label(main_window, text=3, fg="#006400", font=("none", 10, "bold"))
+        label4 = Label(main_window, text=4, fg="#008B8B", font=("none", 10, "bold"))
+        label5 = Label(main_window, text=5, fg="#191970", font=("none", 10, "bold"))
+        label6 = Label(main_window, text=6, fg="#8B4513", font=("none", 10, "bold"))
+        label7 = Label(main_window, text=7, fg="#696969", font=("none", 10, "bold"))
+        label8 = Label(main_window, text=8, fg="#000000", font=("none", 10, "bold"))
+        count = 0
+        surround_list = surround_mine([row, column], grid_height, grid_width)
+        for i in surround_list:
+            if i in mines_list:
+                count += 1
+        if count == 0:
+            label0.grid(row=row, column=column)
+            # super_surround = [surround_list]
+            # checked_list = [[row, column]]
+            # for j in super_surround:
+            #     for k in j:
+            #         new_count = 0
+            #         if k not in checked_list:
+            #             grid[k[0]][k[1]].button.grid_remove()
+            #             new_surround = surround_mine(k, grid_height, grid_width)
+            #             for mine in new_surround:
+            #                 if mine in mines_list:
+            #                     new_count += 1
+            #             if new_count == 0:
+            #                 label0.grid(row=k[0], column=k[1])
+            #                 super_surround.append(new_surround)
+            #             elif new_count == 1:
+            #                 label1.grid(row=k[0], column=k[1])
+            #             elif new_count == 2:
+            #                 label2.grid(row=k[0], column=k[1])
+            #             elif new_count == 3:
+            #                 label3.grid(row=k[0], column=k[1])
+            #             elif new_count == 4:
+            #                 label4.grid(row=k[0], column=k[1])
+            #             elif new_count == 5:
+            #                 label5.grid(row=k[0], column=k[1])
+            #             elif new_count == 6:
+            #                 label6.grid(row=k[0], column=k[1])
+            #             elif new_count == 7:
+            #                 label7.grid(row=k[0], column=k[1])
+            #             elif new_count == 8:
+            #                 label8.grid(row=k[0], column=k[1])
+            #             checked_list.append(k)
+        elif count == 1:
+            label1.grid(row=row, column=column)
+        elif count == 2:
+            label2.grid(row=row, column=column)
+        elif count == 3:
+            label3.grid(row=row, column=column)
+        elif count == 4:
+            label4.grid(row=row, column=column)
+        elif count == 5:
+            label5.grid(row=row, column=column)
+        elif count == 6:
+            label6.grid(row=row, column=column)
+        elif count == 7:
+            label7.grid(row=row, column=column)
+        elif count == 8:
+            label8.grid(row=row, column=column)
+        return False
 
 
 
