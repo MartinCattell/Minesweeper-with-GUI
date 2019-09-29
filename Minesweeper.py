12099f6ba310cd3ad0334fe7e07cb29c54289afc
Truthnuggets <martincattell1618@outlook.com>
2019-09-29 23:49:11 +0100

    "Fix issue with grid resizing on button selection"

--- Minesweeper.py
+++ Minesweeper.py
@@ -1,19 +1,17 @@
 from tkinter import *
 from random import randint
 
-main_window = Tk()
-
 
 class MineButton(object):
-    def __init__(self, row, column):
+    def __init__(self, row, column, window):
         self.row = row
         self.column = column
-        self.button = Button(main_window, width=2, height=1, command=lambda r=self.row, c=self.column: button_function(r, c))
+        self.button = Button(main_window, width=2, height=1, command=lambda r=self.row, c=self.column: button_function(r, c), pady=-1)
 
 
 grid_width = 15
 grid_height = 15
-bomb_percentage = 10
+bomb_percentage = 20
 
 
 def bomb_number(height, width, percent):
@@ -26,7 +24,7 @@ def create_grid(height, width):
     for i in range(height):
         row_list = []
         for j in range(width):
-            b = MineButton(i, j)
+            b = MineButton(i, j, main_window)
             row_list.append(b)
             row_list[j].button.grid(row=i, column=j)
         grid_list.append(row_list)
@@ -47,10 +45,6 @@ def plant_mines(height, width, percent):
     return mines
 
 
-minefield = create_grid(grid_height, grid_width)
-mines_list = plant_mines(grid_height, grid_width, bomb_percentage)
-
-
 def boundary_getter(height, width):
     top_left = [0, 0]
     top_right = [0, width - 1]
@@ -60,15 +54,12 @@ def boundary_getter(height, width):
     top_row = []
     for i in range(1, width - 1):
         top_row.append([0, i])
-
     bottom_row = []
     for i in range(1, width - 1):
         bottom_row.append([width - 1, i])
-
     left_column = []
     for i in range(1, height - 1):
         left_column.append([i, 0])
-
     right_column = []
     for i in range(1, height - 1):
         right_column.append([i, height - 1])
@@ -113,33 +104,35 @@ def mine_counter(coord_list):
 
 
 def count_labeler(count, row, column):
+    minefield[row][column].button.grid_remove()
+    number_border = GROOVE
     if count == 0:
-        label0 = Label(main_window, text="O")
+        label0 = Label(main_window, text="", relief=number_border, width=2, height=1, padx=3, pady=2)
         label0.grid(row=row, column=column)
         return False
     elif count == 1:
-        label1 = Label(main_window, text=1, fg="#FF0000", font=("none", 10, "bold"))
+        label1 = Label(main_window, text=1, fg="#FF0000", font=("none", 9, "bold"), relief=number_border,  width=2, height=1, padx=2)
         label1.grid(row=row, column=column)
     elif count == 2:
-        label2 = Label(main_window, text=2, fg="#FF0000", font=("none", 10, "bold"))
+        label2 = Label(main_window, text=2, fg="#FF0000", font=("none", 9, "bold"), relief=number_border, width=2, height=1, padx=2)
         label2.grid(row=row, column=column)
     elif count == 3:
-        label3 = Label(main_window, text=3, fg="#006400", font=("none", 10, "bold"))
+        label3 = Label(main_window, text=3, fg="#006400", font=("none", 9, "bold"), relief=number_border, width=2, height=1, padx=2)
         label3.grid(row=row, column=column)
     elif count == 4:
-        label4 = Label(main_window, text=4, fg="#008B8B", font=("none", 10, "bold"))
+        label4 = Label(main_window, text=4, fg="#008B8B", font=("none", 9, "bold"), relief=number_border, width=2, height=1, padx=2)
         label4.grid(row=row, column=column)
     elif count == 5:
-        label5 = Label(main_window, text=5, fg="#191970", font=("none", 10, "bold"))
+        label5 = Label(main_window, text=5, fg="#191970", font=("none", 9, "bold"), relief=number_border, width=2, height=1, padx=2)
         label5.grid(row=row, column=column)
     elif count == 6:
-        label6 = Label(main_window, text=6, fg="#8B4513", font=("none", 10, "bold"))
+        label6 = Label(main_window, text=6, fg="#8B4513", font=("none", 9, "bold"), relief=number_border, width=2, height=1, padx=2)
         label6.grid(row=row, column=column)
     elif count == 7:
-        label7 = Label(main_window, text=7, fg="#696969", font=("none", 10, "bold"))
+        label7 = Label(main_window, text=7, fg="#696969", font=("none", 9, "bold"), relief=number_border, width=2, height=1, padx=2)
         label7.grid(row=row, column=column)
     elif count == 8:
-        label8 = Label(main_window, text=8, fg="#000000", font=("none", 10, "bold"))
+        label8 = Label(main_window, text=8, fg="#000000", font=("none", 9, "bold"), relief=number_border, width=2, height=1, padx=2)
         label8.grid(row=row, column=column)
     return True
 
@@ -147,9 +140,12 @@ def count_labeler(count, row, column):
 
 def button_function(row, column):
     if [row, column] in mines_list:
-        label_mine = Label(main_window, text="M", font=("none", 10, "bold"))
         minefield[row][column].button.grid_remove()
+        # mine_image = PhotoImage(Image.open("Mine.gif"))
+        label_mine = Label(main_window, text="M", font=("none", 9, "bold"), width=2, height=1)
         label_mine.grid(row=row, column=column)
+
+        # font=("none", 10, "bold"), width=2, height=1
         return True
     else:
         surround_list = surround_mine([row, column], grid_height, grid_width)
@@ -158,7 +154,6 @@ def button_function(row, column):
             checked_list = [[row, column]]
             super_surround = [surround_list]
             for surr_lst in super_surround:
-                print(checked_list)
                 for mine in surr_lst:
                     if mine not in checked_list:
                         new_surround = surround_mine(mine, grid_height, grid_width)
@@ -168,22 +163,10 @@ def button_function(row, column):
                         checked_list.append(mine)
 
 
+mines_list = plant_mines(grid_height, grid_width, bomb_percentage)
 
+main_window = Tk()
 
-
-
-
-
-
-
-
-
-
-
-
-
-print(mines_list)
-
+minefield = create_grid(grid_height, grid_width)
 main_window.mainloop()
-
 
